cbfa1fbf9d548bb869a8b742a57fb16d71078e3a
sajibprofile <sajibprofile@gmail.com>
2025-05-17 22:01:55 +0600

    Social Link Updated

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ import Photo from "@/components/Photo";
 import Socials from "@/components/Socials";
 import Status from "@/components/Status";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { FiDownload } from "react-icons/fi";
+import {BsArrowBarRight} from "react-icons/bs"
 
 const Home = () => {
   return (
@@ -17,33 +19,38 @@ const Home = () => {
               Hello I'm <br /> <span>Sajib Barua</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 ">
-           I turn digital presence into performance through strategic marketing, SEO mastery, and creative precision.
+              I turn digital presence into performance through strategic
+              marketing, SEO mastery, and creative precision.
             </p>
-          {/* btn and social medias */}
-          <div className="flex flex-col xl:flex-row items-center gap-8 ">
-            <Button 
-            variant="outline"  size="lg" className="uppercase flex items-center gap-2">
-               
-              <span> Download CV</span>
-              <FiDownload />
-            </Button>
-            <div className="mb-8 xl:mb-0">
-              <Socials containerStyles="flex gap-6"
-              iconStyles=" w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 "
-              />
-
+            {/* btn and social medias */}
+            <div className="flex flex-col xl:flex-row items-center gap-8 ">
+              <Link href="/contact">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span> Contact Me</span>
+                   <BsArrowBarRight/>
+                </Button>
+              </Link>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles=" w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 "
+                />
+              </div>
             </div>
-          </div>
           </div>
 
           {/* photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo/>
+            <Photo />
           </div>
         </div>
       </div>
 
-      <Status/>
+      <Status />
     </section>
   );
 };
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,21 +1,24 @@
  import Link from "next/link"
-import {FaGithub, FaLinkedin, FaYoutube, FaTwitter , FaFacebook, FaWhatsapp} from "react-icons/fa"
+import {FaGithub, FaLinkedin, FaYoutube, FaTwitter , FaFacebook, FaWhatsapp,FaInstagram} from "react-icons/fa"
 
 
 const socials = [
-  {icon: <FaGithub/>, path: ""},
-  {icon: <FaLinkedin/>, path: ""},
-  {icon: <FaYoutube/>, path: ""},
-  {icon: <FaTwitter/>, path: ""},
+  {icon: <FaFacebook/>, path: "https://www.facebook.com/searchmavin/"},
+  {icon: <FaLinkedin/>, path: "https://www.youtube.com/user/MyCompanyChannel"},
+  {icon: <FaWhatsapp/>, path: "https://wa.me/8801883168140"},
+  {icon: <FaTwitter/>, path: "https://x.com/Iam_sajibbarua"},
+  {icon: <FaInstagram/>, path: "https://www.instagram.com/shajibsb/?hl=en"},
 ]
 const Socials = ({containerStyles, iconStyles}) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index)=>{
         return(
-          <Link key={index} href={item.path} className={iconStyles} >
-          {item.icon}
-          </Link>
+          // <Link key={index} href={item.path} className={iconStyles} >
+          // {item.icon}
+          // </Link>
+
+          <a href={item.path} key={index} target="_blank"  rel="noopener noreferrer" className={iconStyles} >{item.icon} </a>
         )
       })}
     </div>
